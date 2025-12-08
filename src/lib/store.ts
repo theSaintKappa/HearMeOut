6848d4bb9be74fd47ad29e7902edbec54049cf23
Wojtek Zrałek <wojtek.zralek@icloud.com>
2025-12-08 22:07:35 +0100

    Remove top items fields from store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,29 +1,19 @@
 import { create } from "zustand";
-import type { ContentType, TimeRange, TopItems, ViewMode } from "@/lib/types";
+import type { ContentType, TimeRange, ViewMode } from "@/lib/types";
 
-interface HMOStore extends Record<ContentType, Record<TimeRange, TopItems<ContentType>>> {
+interface HMOStore {
     contentType: ContentType;
     timeRange: TimeRange;
     viewMode: ViewMode;
-    setTopItems: <T extends ContentType>(type: T, range: TimeRange, items: TopItems<T>) => void;
     setContentType: (type: ContentType) => void;
     setTimeRange: (range: TimeRange) => void;
     setViewMode: (mode: ViewMode) => void;
 }
 
-export const useHMOStore = create<HMOStore>((set, get) => ({
-    artists: { short_term: [], medium_term: [], long_term: [] },
-    tracks: { short_term: [], medium_term: [], long_term: [] },
+export const useHMOStore = create<HMOStore>((set) => ({
     contentType: "tracks",
     timeRange: "short_term",
     viewMode: "grid",
-    setTopItems: (type, range, items) =>
-        set({
-            [type]: {
-                ...get()[type],
-                [range]: items,
-            },
-        }),
     setContentType: (type) => set({ contentType: type }),
     setTimeRange: (range) => set({ timeRange: range }),
     setViewMode: (mode) => set({ viewMode: mode }),
